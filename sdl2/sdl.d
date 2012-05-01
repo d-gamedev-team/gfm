fb11d04bcd6b44872638143b0c96a306cfb9300e
ponce <aliloko@gmail.com>
2012-05-01 20:26:46 +0200

    [gfm] implement SDL2 log piping

--- sdl2/sdl.d
+++ sdl2/sdl.d
@@ -1,6 +1,7 @@
 module gfm.sdl2.sdl;
 
-import std.conv;
+import std.conv: to;
+import std.string: format;
 
 import derelict.sdl2.sdl;
 import derelict.sdl2.image;
@@ -13,15 +14,20 @@ final class SDL2
 {
     public
     {
-    /*    string keyName(int key)
-        {
-            return to!string(SDL_GetKeyName(key));
-        }
-
         string getErrorString()
         {
             return to!string(SDL_GetError());
-        }*/
+        }
+
+        string[] getVideoDrivers()
+        {
+            const int numDrivers = SDL_GetNumVideoDrivers();
+            string[] res;
+            res.length = numDrivers;
+            for(int i = 0; i < numDrivers; ++i)
+                res[i] = to!string(SDL_GetVideoDriver(i));
+            return res;
+        }
 
         this(Log log)
         {
@@ -40,21 +46,35 @@ final class SDL2
                 throw new SDL2Exception(e.msg);
             }
 
-            // enable all logging
+            // enable all logging, and pipe it to our own logger object
+            SDL_LogGetOutputFunction(_previousLogCallback, &_previousLogUserdata);
             SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
+            SDL_LogSetOutputFunction(&loggingCallback, cast(void*)this);
 
             if (0 != SDL_Init(0))
-                throw new SDL2Exception("Unable to initialize SDL");
+                throw new SDL2Exception("Unable to initialize SDL: " ~ getErrorString());
 
             subSystemInit(SDL_INIT_TIMER);
             subSystemInit(SDL_INIT_VIDEO);
             subSystemInit(SDL_INIT_JOYSTICK);
             subSystemInit(SDL_INIT_AUDIO);
             subSystemInit(SDL_INIT_HAPTIC);
+
+            foreach(driver;getVideoDrivers())
+                _log.infof("Available driver: %s", driver);
+
+            {
+                int res = SDL_VideoInit(null, 0);
+                if (res != 0)
+                    throw new SDL2Exception("SDL_VideoInit failed: " ~ getErrorString());
+            }
+
+            _log.infof("Running using video driver: %s", to!string(SDL_GetCurrentVideoDriver()));
         }
 
         ~this()
         {
+            SDL_LogSetOutputFunction(_previousLogCallback, _previousLogUserdata);
             SDL_Quit();
         }
     }
@@ -62,6 +82,9 @@ final class SDL2
     private
     {
         Log _log;
+
+        SDL_LogOutputFunction _previousLogCallback;
+        void* _previousLogUserdata;
 
         bool subSystemInitialized(int subSystem)
         {
@@ -75,9 +98,59 @@ final class SDL2
             {
                 int res = SDL_InitSubSystem(flag);
                 if (0 != res)
-                    throw new SDL2Exception("Unable to initialize SDL subsystem.");
+                    throw new SDL2Exception("Unable to initialize SDL subsystem: " ~ getErrorString());
             }
+        }      
+
+        void onLogMessage(int category, SDL_LogPriority priority, const(char)* message)
+        {
+            static string readablePriority(SDL_LogPriority priority) pure
+            {
+                switch(priority)
+                {
+                    case SDL_LOG_PRIORITY_VERBOSE  : return "verbose";
+                    case SDL_LOG_PRIORITY_DEBUG    : return "debug";
+                    case SDL_LOG_PRIORITY_INFO     : return "info";
+                    case SDL_LOG_PRIORITY_WARN     : return "warn";
+                    case SDL_LOG_PRIORITY_ERROR    : return "error";
+                    case SDL_LOG_PRIORITY_CRITICAL : return "critical";
+                    default                        : return "unknown";
+                }
+            }
+
+            static string readableCategory(SDL_LogPriority priority) pure
+            {
+                switch(priority)
+                {
+                    case SDL_LOG_CATEGORY_APPLICATION : return "application";
+                    case SDL_LOG_CATEGORY_ERROR       : return "error";
+                    case SDL_LOG_CATEGORY_SYSTEM      : return "system";
+                    case SDL_LOG_CATEGORY_AUDIO       : return "audio";
+                    case SDL_LOG_CATEGORY_VIDEO       : return "video";
+                    case SDL_LOG_CATEGORY_RENDER      : return "render";
+                    case SDL_LOG_CATEGORY_INPUT       : return "input";
+                    default                           : return "unknown";
+                }
+            }
+
+            string formattedMessage = format("SDL (category %s, priority %s): %s", 
+                                             category, readablePriority(priority), to!string(message));
+
+            if (priority == SDL_LOG_PRIORITY_WARN)
+                _log.warn(formattedMessage);
+            else if (priority == SDL_LOG_PRIORITY_ERROR ||  priority == SDL_LOG_PRIORITY_CRITICAL)
+                _log.error(formattedMessage);
+            else
+                _log.info(formattedMessage);
         }
     }
 }
 
+extern(C)
+{
+    void loggingCallback(void* userData, int category, SDL_LogPriority priority, const(char)* message)
+    {
+        SDL2 sdl2 = cast(SDL2)userData;
+        sdl2.onLogMessage(category, priority, message);
+    }
+}
