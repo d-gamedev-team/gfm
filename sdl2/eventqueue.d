3d1c36154d535e5afa0ca5f882598e1e74dca8f8
ponce <aliloko@gmail.com>
2012-05-12 01:29:17 +0200

    [gfm] add window listener

--- sdl2/eventqueue.d
+++ sdl2/eventqueue.d
@@ -7,23 +7,41 @@ import derelict.sdl2.sdl;
 import gfm.common.log;
 import gfm.sdl2.sdl;
 import gfm.sdl2.exception;
-import gfm.sdl2.window;
 
+interface IWindowListener
+{
+    void onShow();
+    void onHide();
+    void onExposed();
+    void onMove();
+    void onResized();
+    void onSizeChanged();
+    void onMinimized();
+    void onMaximized();
+    void onRestored();
+    void onEnter();
+    void onLeave();
+    void onFocusGained();
+    void onFocusLost();
+    void onClose();
+}
 
+// dispatch events
 final class SDL2EventQueue
 {
     public
     {
-        bool quit = false;
+        
         this(SDL2 sdl2, Log log)
         {
             _sdl2 = sdl2;
             _log = log;
+            _quitWasRequested = false;
         }
 
-        void registerWindow(SDL2Window window)
+        void addWindowListener(uint id, IWindowListener windowListener)
         {
-            _knownWindows ~= window;
+            _windowListeners[id] = windowListener;
         }
 
         void processEvents()
@@ -31,17 +49,12 @@ final class SDL2EventQueue
             SDL_Event event;
 
             while(SDL_PollEvent(&event) != 0)
-            {
-                switch(event.type)
-                {
-                    case SDL_QUIT: 
-                        quit = true;
-                        _log.info("quit");
-                        break;
-                    default:
-                        break;
-                }
-            }
+                dispatch(&event);
+        }
+
+        bool wasQuitResquested() const
+        {
+            return _quitWasRequested;
         }
     }
 
@@ -49,7 +62,113 @@ final class SDL2EventQueue
     {
         SDL2 _sdl2;
         Log _log;
-        SDL2Window[] _knownWindows;
+        IWindowListener[uint] _windowListeners;
+        bool _quitWasRequested = false;
 
+        void dispatch(const (SDL_Event*) event)
+        {
+            switch(event.type)
+            {
+                case SDL_QUIT: 
+                    _quitWasRequested = true;
+                    break;
+
+                case SDL_WINDOWEVENT:
+                    dispatchWindowEvent(&event.window);
+                    break;
+
+                case SDL_KEYDOWN:
+                case SDL_KEYUP:
+                //case SDL_TEXTEDITING:
+                //case SDL_TEXTINPUT:
+                
+                case SDL_MOUSEMOTION:
+                case SDL_MOUSEBUTTONDOWN:
+                case SDL_MOUSEBUTTONUP:
+                case SDL_MOUSEWHEEL:
+
+                case SDL_JOYAXISMOTION:
+                case SDL_JOYBALLMOTION:
+                case SDL_JOYHATMOTION:
+                case SDL_JOYBUTTONDOWN:
+                case SDL_JOYBUTTONUP:                
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+
+        void dispatchWindowEvent(const (SDL_WindowEvent*) windowEvent)
+        {
+            assert(windowEvent.type == SDL_WINDOWEVENT);
+
+            IWindowListener* listener = (windowEvent.windowID in _windowListeners);
+
+            if (listener is null)
+                return; // no such id known
+
+            switch (windowEvent.event)
+            {
+                case SDL_WINDOWEVENT_SHOWN:
+                    listener.onShow();
+                    break;
+
+                case SDL_WINDOWEVENT_HIDDEN:
+                    listener.onHide();
+                    break;
+
+                case SDL_WINDOWEVENT_EXPOSED:
+                    listener.onExposed();
+                    break;
+
+                case SDL_WINDOWEVENT_MOVED:
+                    listener.onMove();
+                    break;
+
+                case SDL_WINDOWEVENT_RESIZED:
+                    listener.onResized();
+                    break;
+
+                case SDL_WINDOWEVENT_SIZE_CHANGED:
+                    listener.onSizeChanged();
+                    break;
+
+                case SDL_WINDOWEVENT_MINIMIZED:
+                    listener.onMinimized();
+                    break;
+
+                case SDL_WINDOWEVENT_MAXIMIZED:
+                    listener.onMaximized();
+                    break;
+
+                case SDL_WINDOWEVENT_RESTORED:
+                    listener.onRestored();
+                    break;
+
+                case SDL_WINDOWEVENT_ENTER:
+                    listener.onEnter();
+                    break;
+
+                case SDL_WINDOWEVENT_LEAVE:
+                    listener.onLeave();
+                    break;
+
+                case SDL_WINDOWEVENT_FOCUS_GAINED:
+                    listener.onFocusGained();
+                    break;
+
+                case SDL_WINDOWEVENT_FOCUS_LOST:
+                    listener.onFocusLost();
+                    break;
+
+                case SDL_WINDOWEVENT_CLOSE:
+                    listener.onClose();
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 }
