f37c68d5cf4a6a64dffe995ed4d0241cfb4ef802
ponce <aliloko@gmail.com>
2012-05-18 22:36:39 +0200

    remove event listener

--- sdl2/all.d
+++ sdl2/all.d
@@ -6,7 +6,6 @@ public
     import gfm.sdl2.sdl;
     import gfm.sdl2.surface;
     import gfm.sdl2.displaymode;
-    import gfm.sdl2.eventlisteners;
     import gfm.sdl2.window;
     import gfm.sdl2.eventqueue;    
     import gfm.sdl2.keyboard;
