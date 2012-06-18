09b3ef60efc63a47e74bd780e139f06b42284278
ponce <aliloko@gmail.com>
2012-06-19 00:05:33 +0200

    added OpenGL shader wrapper

---
+++ opengl/shader.d
@@ -0,0 +1,103 @@
+module gfm.opengl.shader;
+
+import std.string;
+import std.conv;
+
+import derelict.opengl3.gl3;
+
+import gfm.common.log;
+import gfm.opengl.opengl;
+import gfm.opengl.exception;
+
+
+final class GLShader
+{
+    public
+    {
+        this(GLenum shaderType)
+        {
+            _shader = glCreateShader(shaderType);
+            if (_shader == 0)
+                throw new OpenGLException("glCreateShader failed");
+            _initialized = true;
+        }
+
+        ~this()
+        {
+            close();
+        }
+
+        void close()
+        {
+            if (_initialized)
+            {
+                glDeleteShader(_shader);
+                _initialized = false;
+            }
+        }
+
+        void load(string source)
+        {
+            load( [source] );
+        }
+
+        void load(string[] lines)
+        {
+            size_t lineCount = lines.length;
+
+            auto lengths = new GLint[lineCount];
+            auto addresses = new immutable(GLchar)*[lineCount];
+            auto localLines = new string[lineCount];
+            
+            for (size_t i = 0; i < lineCount; ++i)
+            {
+                localLines[i] = lines[i];
+                if (localLines[i] is null)
+                    localLines[i] = "";
+
+                lengths[i] = localLines[i].length;
+                addresses[i] = localLines[i].ptr;
+            }
+
+            glShaderSource(_shader, 
+                           cast(GLint)lineCount, 
+                           cast(const(char)**)addresses.ptr, 
+                           cast(const(int)*)(lengths.ptr));
+            _gl.runtimeCheck();
+        }
+
+        void compile()
+        {
+            glCompileShader(_shader);
+            _gl.runtimeCheck();
+            
+            GLint compiled;
+            glGetShaderiv(_shader, GL_COMPILE_STATUS, &compiled);
+            
+            if (compiled != GL_TRUE)
+                throw new OpenGLException("shader did not compile");
+        }
+
+        string getInfoLog()
+        {
+            GLint logLength;
+            glGetShaderiv(_shader, GL_INFO_LOG_LENGTH, &logLength);
+            char[] log = new char[logLength + 1];
+            GLint dummy;
+            glGetShaderInfoLog(_shader, logLength, &dummy, log.ptr);
+            _gl.debugCheck();
+            return to!string(log.ptr);
+        }
+    }
+
+    package
+    {
+        GLuint _shader;
+    }
+
+    private
+    {
+        OpenGL _gl;
+        bool _initialized;
+    }
+}
