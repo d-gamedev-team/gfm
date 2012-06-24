4b7b2e98c496309b3446b2ef517ce22ec75e5d77
ponce <aliloko@gmail.com>
2012-06-24 23:13:45 +0200

    add FANN wrapper

---
+++ fann/all.d
@@ -0,0 +1,9 @@
+module gfm.fann.all;
+
+public
+{
+    import derelict.fann.fann;
+    import gfm.fann.lib;
+    import gfm.fann.fann;
+}
+
---
+++ fann/fann.d
@@ -0,0 +1,109 @@
+module gfm.fann.fann;
+
+import derelict.fann.fann;
+import gfm.fann.lib;
+
+// ANN stands for Artificial Neural Network
+final class ANN
+{
+    public
+    {
+        static ANN createStandard(FANNLib lib, uint[] layers)
+        {
+            FANN* fann = fann_create_standard_array(layers.length, layers.ptr);
+            lib.runtimeCheck();
+            assert(fann !is null);
+            return new ANN(lib, fann);
+        }
+
+        static ANN createSparse(FANNLib lib, float connectionRate, uint[] layers)
+        {
+            FANN* fann = fann_create_sparse_array(connectionRate, layers.length, layers.ptr);
+            lib.runtimeCheck();
+            assert(fann !is null);
+            return new ANN(lib, fann);
+        }
+
+        static ANN createSparse(FANNLib lib, uint[] layers)
+        {
+            FANN* fann = fann_create_shortcut_array(layers.length, layers.ptr);
+            lib.runtimeCheck();
+            assert(fann !is null);
+            return new ANN(lib, fann);
+        }
+
+        ~this()
+        {
+            close();
+        }
+        
+        void close()
+        {
+            if (_fann !is null)
+            {
+                fann_destroy(_fann);
+                _fann = null;
+            }
+        }
+
+        fann_type* run(fann_type*	input)
+        {
+            fann_type* res = fann_run(_fann, input);
+            _lib.runtimeCheck();
+            return res;
+        }
+
+        void printConnections()
+        {
+            fann_print_connections(_fann);
+        }
+
+        void printParameters()
+        {
+            fann_print_parameters(_fann);
+        }
+
+        void randomizeWeights(fann_type minWeight, fann_type maxWeight)
+        {
+            fann_randomize_weights(_fann, minWeight, maxWeight);
+            _lib.runtimeCheck();
+        }
+
+        uint numInputs()
+        {
+            return fann_get_num_input(_fann);
+        }
+
+        uint numOutputs()
+        {
+            return fann_get_num_output(_fann);
+        }
+
+        uint numLayers()
+        {
+            return fann_get_num_layers(_fann);
+        }
+
+        uint totalNeurons()
+        {
+            return fann_get_total_neurons(_fann);
+        }
+
+        uint totalConnections()
+        {
+            return fann_get_total_connections(_fann);
+        }
+    }
+
+    private
+    {
+        this(FANNLib lib, FANN* fann)
+        {
+            _lib = lib;
+            _fann = fann;
+        }
+
+        FANN* _fann;
+        FANNLib _lib;
+    }
+}
