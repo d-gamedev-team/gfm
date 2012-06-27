bdfb7424a5644def24758996c7d7b4b77e16d0ec
ponce <aliloko@gmail.com>
2012-06-27 23:14:47 +0200

    better uniform type error on assignment

--- opengl/program.d
+++ opengl/program.d
@@ -208,7 +208,7 @@ final class GLUniform
                 return;
 
             if (!typeIsCompliant!T(_type))
-                throw new OpenGLException(format("using type %s for setting uniform '%s' which has type %s", T.stringof, _name, _type.stringof));
+                throw new OpenGLException(format("using type %s for setting uniform '%s' which has GLSL type '%s'", T.stringof, _name, GLSLTypeNameArray(_type, _size)));
 
             if (newValue.length != _size)
                 throw new OpenGLException(format("cannot set uniform '%s' of size %s with a value of size %s", _name, _size, newValue.length));
@@ -283,21 +283,21 @@ final class GLUniform
                 case GL_FLOAT_MAT2: return is(T == mat2f);
                 case GL_FLOAT_MAT3: return is(T == mat3f);
                 case GL_FLOAT_MAT4: return is(T == mat4f);
-                case GL_FLOAT_MAT2x3: return is(T == mat2x3f);
-                case GL_FLOAT_MAT2x4: return is(T == mat2x4f);
-                case GL_FLOAT_MAT3x2: return is(T == mat3x2f);
-                case GL_FLOAT_MAT3x4: return is(T == mat3x4f);
-                case GL_FLOAT_MAT4x2: return is(T == mat4x2f);
-                case GL_FLOAT_MAT4x3: return is(T == mat4x3f);
+                case GL_FLOAT_MAT2x3: return is(T == mat3x2f);
+                case GL_FLOAT_MAT2x4: return is(T == mat4x2f);
+                case GL_FLOAT_MAT3x2: return is(T == mat2x3f);
+                case GL_FLOAT_MAT3x4: return is(T == mat4x3f);
+                case GL_FLOAT_MAT4x2: return is(T == mat2x4f);
+                case GL_FLOAT_MAT4x3: return is(T == mat3x4f);
                 case GL_DOUBLE_MAT2: return is(T == mat2d);
                 case GL_DOUBLE_MAT3: return is(T == mat3d);
                 case GL_DOUBLE_MAT4: return is(T == mat4d);
-                case GL_DOUBLE_MAT2x3: return is(T == mat2x3d);
-                case GL_DOUBLE_MAT2x4: return is(T == mat2x4d);
-                case GL_DOUBLE_MAT3x2: return is(T == mat3x2d);
-                case GL_DOUBLE_MAT3x4: return is(T == mat3x4d);
-                case GL_DOUBLE_MAT4x2: return is(T == mat4x2d);
-                case GL_DOUBLE_MAT4x3: return is(T == mat4x3d);
+                case GL_DOUBLE_MAT2x3: return is(T == mat3x2d);
+                case GL_DOUBLE_MAT2x4: return is(T == mat4x2d);
+                case GL_DOUBLE_MAT3x2: return is(T == mat2x3d);
+                case GL_DOUBLE_MAT3x4: return is(T == mat4x3d);
+                case GL_DOUBLE_MAT4x2: return is(T == mat2x4d);
+                case GL_DOUBLE_MAT4x3: return is(T == mat3x4d);
 
                 // image samplers
                 case GL_IMAGE_1D: .. case GL_UNSIGNED_INT_IMAGE_2D_MULTISAMPLE_ARRAY:
@@ -384,21 +384,21 @@ final class GLUniform
                 case GL_FLOAT_MAT2: return mat2f.sizeof;
                 case GL_FLOAT_MAT3: return mat3f.sizeof;
                 case GL_FLOAT_MAT4: return mat4f.sizeof;
-                case GL_FLOAT_MAT2x3: return mat2x3f.sizeof;
-                case GL_FLOAT_MAT2x4: return mat2x4f.sizeof;
-                case GL_FLOAT_MAT3x2: return mat3x2f.sizeof;
-                case GL_FLOAT_MAT3x4: return mat3x4f.sizeof;
-                case GL_FLOAT_MAT4x2: return mat4x2f.sizeof;
-                case GL_FLOAT_MAT4x3: return mat4x3f.sizeof;
+                case GL_FLOAT_MAT2x3: return mat3x2f.sizeof;
+                case GL_FLOAT_MAT2x4: return mat4x2f.sizeof;
+                case GL_FLOAT_MAT3x2: return mat2x3f.sizeof;
+                case GL_FLOAT_MAT3x4: return mat4x3f.sizeof;
+                case GL_FLOAT_MAT4x2: return mat2x4f.sizeof;
+                case GL_FLOAT_MAT4x3: return mat3x4f.sizeof;
                 case GL_DOUBLE_MAT2: return mat2d.sizeof;
                 case GL_DOUBLE_MAT3: return mat3d.sizeof;
                 case GL_DOUBLE_MAT4: return mat4d.sizeof;
-                case GL_DOUBLE_MAT2x3: return mat2x3d.sizeof;
-                case GL_DOUBLE_MAT2x4: return mat2x4d.sizeof;
-                case GL_DOUBLE_MAT3x2: return mat3x2d.sizeof;
-                case GL_DOUBLE_MAT3x4: return mat3x4d.sizeof;
-                case GL_DOUBLE_MAT4x2: return mat4x2d.sizeof;
-                case GL_DOUBLE_MAT4x3: return mat4x3d.sizeof;
+                case GL_DOUBLE_MAT2x3: return mat3x2d.sizeof;
+                case GL_DOUBLE_MAT2x4: return mat4x2d.sizeof;
+                case GL_DOUBLE_MAT3x2: return mat2x3d.sizeof;
+                case GL_DOUBLE_MAT3x4: return mat4x3d.sizeof;
+                case GL_DOUBLE_MAT4x2: return mat2x4d.sizeof;
+                case GL_DOUBLE_MAT4x3: return mat3x4d.sizeof;
 
                     // image samplers
                 case GL_IMAGE_1D: .. case GL_UNSIGNED_INT_IMAGE_2D_MULTISAMPLE_ARRAY:
@@ -457,6 +457,129 @@ final class GLUniform
                         return 0;
                     }
             }
+        }
+
+        static string GLSLTypeName(GLenum type)
+        {
+            switch (type)
+            {
+                case GL_FLOAT: return "float";
+                case GL_FLOAT_VEC2: return "vec2";
+                case GL_FLOAT_VEC3: return "vec3";
+                case GL_FLOAT_VEC4: return "vec4";
+                case GL_DOUBLE: return "double";
+                case GL_DOUBLE_VEC2: return "dvec2";
+                case GL_DOUBLE_VEC3: return "dvec3";
+                case GL_DOUBLE_VEC4: return "dvec4";
+                case GL_INT: return "int";
+                case GL_INT_VEC2: return "ivec2";
+                case GL_INT_VEC3: return "ivec3";
+                case GL_INT_VEC4: return "ivec4";
+                case GL_UNSIGNED_INT: return "uint";
+                case GL_UNSIGNED_INT_VEC2: return "uvec2";
+                case GL_UNSIGNED_INT_VEC3: return "uvec3";
+                case GL_UNSIGNED_INT_VEC4: return "uvec4";
+                case GL_BOOL: return "bool";
+                case GL_BOOL_VEC2: return "bvec2";
+                case GL_BOOL_VEC3: return "bvec3";
+                case GL_BOOL_VEC4: return "bvec4";
+                case GL_FLOAT_MAT2: return "mat2";
+                case GL_FLOAT_MAT3: return "mat3";
+                case GL_FLOAT_MAT4: return "mat4";
+                case GL_FLOAT_MAT2x3: return "mat2x3";
+                case GL_FLOAT_MAT2x4: return "mat2x4";
+                case GL_FLOAT_MAT3x2: return "mat3x2";
+                case GL_FLOAT_MAT3x4: return "mat3x4";
+                case GL_FLOAT_MAT4x2: return "mat4x2";
+                case GL_FLOAT_MAT4x3: return "mat4x3";
+                case GL_DOUBLE_MAT2: return "dmat2";
+                case GL_DOUBLE_MAT3: return "dmat3";
+                case GL_DOUBLE_MAT4: return "dmat4";
+                case GL_DOUBLE_MAT2x3: return "dmat2x3";
+                case GL_DOUBLE_MAT2x4: return "dmat2x4";
+                case GL_DOUBLE_MAT3x2: return "dmat3x2";
+                case GL_DOUBLE_MAT3x4: return "dmat3x4";
+                case GL_DOUBLE_MAT4x2: return "dmat4x2";
+                case GL_DOUBLE_MAT4x3: return "dmat4x3";
+                case GL_SAMPLER_1D: return "sampler1D";
+                case GL_SAMPLER_2D: return "sampler2D?";
+                case GL_SAMPLER_3D: return "sampler3D";
+                case GL_SAMPLER_CUBE: return "samplerCube?";
+                case GL_SAMPLER_1D_SHADOW: return "sampler1DShadow?";
+                case GL_SAMPLER_2D_SHADOW: return "sampler2DShadow?";
+                case GL_SAMPLER_1D_ARRAY: return "sampler1DArray?";
+                case GL_SAMPLER_2D_ARRAY: return "sampler2DArray?";
+                case GL_SAMPLER_1D_ARRAY_SHADOW: return "sampler1DArrayShadow?";
+                case GL_SAMPLER_2D_ARRAY_SHADOW: return "sampler2DArrayShadow?";
+                case GL_SAMPLER_2D_MULTISAMPLE: return "sampler2DMS?";
+                case GL_SAMPLER_2D_MULTISAMPLE_ARRAY: return "sampler2DMSArray?";
+                case GL_SAMPLER_CUBE_SHADOW: return "samplerCubeShadow?";
+                case GL_SAMPLER_BUFFER: return "samplerBuffer?";
+                case GL_SAMPLER_2D_RECT: return "sampler2DRect?";
+                case GL_SAMPLER_2D_RECT_SHADOW: return "sampler2DRectShadow?";
+                case GL_INT_SAMPLER_1D: return "isampler1D?";
+                case GL_INT_SAMPLER_2D: return "isampler2D?";
+                case GL_INT_SAMPLER_3D: return "isampler3D?";
+                case GL_INT_SAMPLER_CUBE: return "isamplerCube?";
+                case GL_INT_SAMPLER_1D_ARRAY: return "isampler1DArray?";
+                case GL_INT_SAMPLER_2D_ARRAY: return "isampler2DArray?";
+                case GL_INT_SAMPLER_2D_MULTISAMPLE: return "isampler2DMS?";
+                case GL_INT_SAMPLER_2D_MULTISAMPLE_ARRAY: return "isampler2DMSArray?";
+                case GL_INT_SAMPLER_BUFFER: return "isamplerBuffer?";
+                case GL_INT_SAMPLER_2D_RECT: return "isampler2DRect?";
+                case GL_UNSIGNED_INT_SAMPLER_1D: return "usampler1D?";
+                case GL_UNSIGNED_INT_SAMPLER_2D: return "usampler2D?";
+                case GL_UNSIGNED_INT_SAMPLER_3D: return "usampler3D?";
+                case GL_UNSIGNED_INT_SAMPLER_CUBE: return "usamplerCube?";
+                case GL_UNSIGNED_INT_SAMPLER_1D_ARRAY: return "usampler2DArray?";
+                case GL_UNSIGNED_INT_SAMPLER_2D_ARRAY: return "usampler2DArray?";
+                case GL_UNSIGNED_INT_SAMPLER_2D_MULTISAMPLE: return "usampler2DMS?";
+                case GL_UNSIGNED_INT_SAMPLER_2D_MULTISAMPLE_ARRAY: return "usampler2DMSArray?";
+                case GL_UNSIGNED_INT_SAMPLER_BUFFER: return "usamplerBuffer?";
+                case GL_UNSIGNED_INT_SAMPLER_2D_RECT: return "usampler2DRect?";
+                case GL_IMAGE_1D: return "image1D?";
+                case GL_IMAGE_2D: return "image2D?";
+                case GL_IMAGE_3D: return "image3D?";
+                case GL_IMAGE_2D_RECT: return "image2DRect?";
+                case GL_IMAGE_CUBE: return "imageCube?";
+                case GL_IMAGE_BUFFER: return "imageBuffer?";
+                case GL_IMAGE_1D_ARRAY: return "image1DArray?";
+                case GL_IMAGE_2D_ARRAY: return "image2DArray?";
+                case GL_IMAGE_2D_MULTISAMPLE: return "image2DMS?";
+                case GL_IMAGE_2D_MULTISAMPLE_ARRAY: return "image2DMSArray?";
+                case GL_INT_IMAGE_1D: return "iimage1D?";
+                case GL_INT_IMAGE_2D: return "iimage2D?";
+                case GL_INT_IMAGE_3D: return "iimage3D?";
+                case GL_INT_IMAGE_2D_RECT: return "iimage2DRect?";
+                case GL_INT_IMAGE_CUBE: return "iimageCube?";
+                case GL_INT_IMAGE_BUFFER: return "iimageBuffer?";
+                case GL_INT_IMAGE_1D_ARRAY: return "iimage1DArray?";
+                case GL_INT_IMAGE_2D_ARRAY: return "iimage2DArray?";
+                case GL_INT_IMAGE_2D_MULTISAMPLE: return "iimage2DMS?";
+                case GL_INT_IMAGE_2D_MULTISAMPLE_ARRAY: return "iimage2DMSArray?";
+                case GL_UNSIGNED_INT_IMAGE_1D: return "uimage1D?";
+                case GL_UNSIGNED_INT_IMAGE_2D: return "uimage2D?";
+                case GL_UNSIGNED_INT_IMAGE_3D: return "uimage3D?";
+                case GL_UNSIGNED_INT_IMAGE_2D_RECT: return "uimage2DRect?";
+                case GL_UNSIGNED_INT_IMAGE_CUBE: return "uimageCube?";
+                case GL_UNSIGNED_INT_IMAGE_BUFFER: return "uimageBuffer?";
+                case GL_UNSIGNED_INT_IMAGE_1D_ARRAY: return "uimage1DArray?";
+                case GL_UNSIGNED_INT_IMAGE_2D_ARRAY: return "uimage2DArray?";
+                case GL_UNSIGNED_INT_IMAGE_2D_MULTISAMPLE: return "uimage2DMS?";
+                case GL_UNSIGNED_INT_IMAGE_2D_MULTISAMPLE_ARRAY: return "uimage2DMSArray?";
+                case GL_UNSIGNED_INT_ATOMIC_COUNTER: return "atomic_uint?";
+                default:
+                    return "unknown";
+            }
+        }
+
+        static string GLSLTypeNameArray(GLenum type, size_t multiplicity)
+        {
+            assert(multiplicity > 0);
+            if (multiplicity == 1)
+                return GLSLTypeName(type);
+            else
+                return format("%s[%s]", GLSLTypeName(type), multiplicity);
         }
     }
 }
