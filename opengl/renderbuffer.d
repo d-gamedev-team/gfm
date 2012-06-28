d6ed2fc1393e1b75fbf2989e06f632a3d182f62d
ponce <aliloko@gmail.com>
2012-06-28 12:15:45 +0200

    support for multisampled renderbuffer with robust creation and fallback to non-multisampled

--- opengl/renderbuffer.d
+++ opengl/renderbuffer.d
@@ -1,15 +1,18 @@
 module gfm.opengl.renderbuffer;
 
+import std.string;
+
 import derelict.opengl3.gl3;
 
 import gfm.common.log;
-import gfm.opengl.opengl;
+import gfm.math.funcs;
+import gfm.opengl.opengl, gfm.opengl.exception;
 
 final class RenderBuffer
 {
     public
     {
-        this(OpenGL gl, GLenum internalFormat, int width, int height)
+        this(OpenGL gl, GLenum internalFormat, int width, int height, int samples = 0)
         {
             _gl = gl;
             glGenRenderbuffers(1, &_handle);
@@ -17,7 +20,44 @@ final class RenderBuffer
             
             use();
             scope(exit) unuse();
-            glRenderbufferStorage(GL_RENDERBUFFER, internalFormat, width, height);
+            if (samples > 1)
+            {
+                // fallback to non multisampled
+                if (glRenderbufferStorageMultisample is null)
+                {
+                    gl._log.warnf("render-buffer multisampling is not supported, fallback to non-multisampled");
+                    goto non_mutisampled;
+                }
+                
+                int maxSamples;
+                glGetIntegerv(GL_MAX_SAMPLES, &maxSamples);
+                if (maxSamples < 1)
+                    maxSamples = 1;
+
+                // limit samples to what is supported on this machine
+                if (samples >= maxSamples)
+                {
+                    int newSamples = clamp(samples, 0, maxSamples - 1);
+                    gl._log.warnf(format("implementation does not support %s samples, fallback to %s samples", samples, newSamples));
+                    samples = newSamples;
+                }
+
+                try
+                {
+                    glRenderbufferStorageMultisample(GL_RENDERBUFFER, samples, internalFormat, width, height);
+                }
+                catch(OpenGLException e)
+                {
+                    _gl._log.warn(e.msg);
+                    goto non_mutisampled; // fallback to non multisampled
+                }
+            }
+            else
+            {
+            non_mutisampled:
+                glRenderbufferStorage(GL_RENDERBUFFER, internalFormat, width, height);
+                gl.runtimeCheck();
+            }
 
             _initialized = true;
         }
