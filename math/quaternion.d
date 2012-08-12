4461222a7d168ce65357c55b4ebee7cf5eaff107
ponce <aliloko@gmail.com>
2012-08-12 13:27:36 +0200

    oups

--- math/quaternion.d
+++ math/quaternion.d
@@ -99,24 +99,24 @@ nothrow:
             return opEquals(conv);
         }
 
-        // convert to 3x3 matrix
-        U opCast(U)() pure nothrow const if (is(U == mat3!(T)))
+        // convert to 3x3 rotation matrix
+        U opCast(U)() pure nothrow const if (is(Unqual!U == mat3!T))
         {
-            T xx = x * x,
-              xy = x * y,
-              xz = x * z,
-              xw = x * w,
-              yy = y * y,
-              yz = y * z,
-              yw = y * w,
-              zz = z * z,
-              zw = z * w;
+            T xx = x * x, xy = x * y, xz = x * z, xw = x * w,
+                          yy = y * y, yz = y * z, yw = y * w,
+                                      zz = z * z, zw = z * w;
             return mat3!(T)
             (
                 1 - 2 * (yy + zz)    , 2 * (xy - zw)        , 2 * (xz + yw)    ,
                 2 * (xy + zw)        , 1 - 2 * (xx + zz)    , 2 * (yz - xw)    ,
                 2 * (xz - yw)        , 2 * (yz + xw)        , 1 - 2 * (xx + yy)
             );
+        }
+
+        // convert to 4x4 rotation matrix
+        U opCast(U)() pure nothrow const if (is(Unqual!U == mat4!T))
+        {
+            return cast(mat4!T)(cast(mat3!T)(this));
         }
     }
 
