74a432a241f06b755fbac7bfe315066535d14fd7
ponce <ponce@Macintosh-4.local>
2012-09-05 00:08:52 +0200

    Update mac build script

--- mac_build.sh
+++ mac_build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rdmd -I.. -I../Derelict3/import -I~/d/phobos/ -I~/d/druntime/src/ examples/simplegl.d
+rdmd -g -debug -unittest -w -vtls -I.. -I../Derelict3/import -I~/d/phobos/ -I~/d/druntime/src/ examples/simplegl.d --force --chatty
