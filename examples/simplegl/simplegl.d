43fa76831cac7ff58fbd65273fdb5b45038430e7
ponce <aliloko@gmail.com>
2013-09-15 21:33:46 +0200

    update example

--- examples/simplegl/simplegl.d
+++ examples/simplegl/simplegl.d
@@ -1,101 +1,68 @@
-import std.stdio;
-import std.math;
-import gfm.sdl2.all;
-import gfm.common.all;
-import gfm.opengl.all;
+import std.math,
+       std.typecons;
 
+import gfm.common.all,
+       gfm.sdl2.all,
+       gfm.opengl.all;
 
-class MyWindow : SDL2Window
-{
-    this(SDL2 sdl2, box2i position, bool fullscreen)
-    {
-        super(sdl2, position, fullscreen, true, false);
-    }
-}
+// This example show how to draw an OpenGL triangle
 
 void main()
 {
-    auto log = new ConsoleLog();
+    int width = 1280;
+    int height = 720;
+    double ratio = width / cast(double)height;
 
-    auto sdl2 = new SDL2(log);
-    scope(exit) sdl2.close();
+    // create a default logger
+    auto log = defaultLog();
 
-    auto gl = new OpenGL(log);
-    scope(exit) gl.close();
+    // load dynamic libraries
+    auto sdl2 = scoped!SDL2(log);
+    auto gl = scoped!OpenGL(log);
 
-    auto displays = sdl2.getDisplays();
+    // create an OpenGL-enabled SDL window
+    auto window = scoped!SDL2Window(sdl2, 
+                                    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                    width, height,
+                                    SDL_WINDOW_OPENGL);
 
-    if (displays.length == 0)
-        return; // no display
-
-    auto bounds = displays[0].bounds().shrink(vec2i(40));
-
-    auto window = new MyWindow(sdl2, bounds, false);
-    scope(exit) window.close();
-
-    auto eventQueue = new SDL2EventQueue(sdl2);
+    // create an event queue and register that window
+    auto eventQueue = scoped!SDL2EventQueue(sdl2);
     eventQueue.registerWindow(window);
 
-    FrameCounter fc = new FrameCounter(sdl2);
-
+    // reload OpenGL now that a context exists
     gl.reload();
 
-    auto shader = new GLShader(gl, GL_FRAGMENT_SHADER, 
-    [
-    r"uniform vec4 col;
-
-      void main()
-      {
-          gl_FragColor = col;
-      }
-     "]);
-
-    auto program = new GLProgram(gl, shader);
-   
-    double t = 0;
+    double time = 0;
     while(!eventQueue.keyboard().isPressed(SDLK_ESCAPE))
     {
         eventQueue.processEvents();
 
-        auto dt = fc.tickMs();
+        time += 0.10;
 
-        glViewport(0, 0, bounds.width, bounds.height);
+        // clear the whole window
+        glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // load projection and model-view matrices
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-ratio, +ratio, -1.0, 1.0, -1.0, 1.0);
 
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
+        glRotatef(time * 10, 0, 0, 1);
 
-        glDisable(GL_DEPTH_TEST);
-        t += dt * 0.001;
-
-        glBegin(GL_QUADS);
-        glColor3f(1,0,0);
-        glVertex2f(-1,-1);
-        glColor3f(0,0,sin(t));
-        glVertex2f(+1,-1);
-        glColor3f(1,cos(t),1);
-        glVertex2f(+1,+1);
-        glColor3f(1,1,cos(t));
-        glVertex2f(-1,+1);
-        glEnd();
-
-        program.uniform("col").set(vec4f(1.0f, 0.5f, 0.3f, 0.5f));
-        program.use();
-
-        glRotatef(t * 10, 0, 0, 1);
+        // draw a single triangle
         glBegin(GL_TRIANGLES);
-        glColor3f(0,0,0);
-        glVertex2f(-0.5,-0.5);
-        glVertex2f(+0.5,-0.5);
-        glColor3f(1,1,1);
-        glVertex2f(0,+1.5);
+            glColor3f(1.0f, 0.0f, 0.0f);
+            glVertex2f(-0.5, -0.5);
+            glColor3f(0.0f, 1.0f, 0.0f);
+            glVertex2f(+0.5f, -0.5f);
+            glColor3f(0.0f, 0.0f, 1.0f);
+            glVertex2f(0.0f, 0.6f);
         glEnd();
 
-        program.unuse();
-        
-        window.setTitle(fc.getFPSString());
-        window.swapBuffers();        
+        window.swapBuffers();
     }
 }
