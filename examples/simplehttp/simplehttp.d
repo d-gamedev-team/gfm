40b044c280855b2433d21144129cbbd96d3a8ea5
ponce <aliloko@gmail.com>
2013-12-01 21:07:21 +0100

    Update GFM examples to newest dub. The 'simplehttp' example now prints headers and body of GET performed on a user-provided URL.

--- examples/simplehttp/simplehttp.d
+++ examples/simplehttp/simplehttp.d
@@ -1,8 +1,32 @@
 import gfm.net.all;
 
+import std.stdio;
 
-void main()
+// Print the result of a GET request
+
+void main(string args[])
 {
-    auto client = new HTTPClient();
-    HTTPResponse response = client.GET(new URI("http://google.com"));
+    if (args.length != 2)
+    {
+        writefln("Usage: simplehttp http://www.myurl.com");
+        writefln("       Print the result of a GET request.");
+
+        return;
+    }
+
+    string url = args[1];
+
+    scope auto client = new HTTPClient();
+    HTTPResponse response = client.GET(new URI(url));
+
+    // Write headers
+    writefln("%s returned error code %s", url, response.statusCode );
+
+    foreach (string header, value; response.headers)
+    {
+        writefln("%s: %s", header, value);
+    }
+
+    writefln("Body: \n");
+    writefln("%s", response.content);
 }
