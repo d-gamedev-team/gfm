75f32ebd8a63ab51cc1256de2f8cf58b62f2ff3c
ponce <aliloko@gmail.com>
2013-12-01 22:05:04 +0100

    add index.d

---
+++ source/index.d
@@ -0,0 +1,8 @@
+Ddoc
+
+$(LINK2 https://github.com/p0nce/gfm, gfm) is a feature-rich library to ease the creation of video games / multimedia applications with the D programming language. Pick what you need.
+
+License:
+  Public domain.
+
+  
