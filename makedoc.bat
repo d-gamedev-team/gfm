8ebe4078b20db5d16f38aedfe0770e739dc2b650
ponce <aliloko@gmail.com>
2013-12-30 17:50:34 +0100

    Update doc generation script, restore the older directory structure before running doc generation.

--- makedoc.bat
+++ makedoc.bat
@@ -1,5 +1,15 @@
 
 :lol
-rdmd bootDoc\generate.d ..\gfm\source
+xcopy /Y /e ..\gfm\index.d source
+xcopy /Y /e ..\gfm\core source
+xcopy /Y /e ..\gfm\math source
+xcopy /Y /e ..\gfm\image source
+xcopy /Y /e ..\gfm\net source
+xcopy /Y /e ..\gfm\sdl2 source
+xcopy /Y /e ..\gfm\opengl source
+xcopy /Y /e ..\gfm\assimp source
+xcopy /Y /e ..\gfm\freeimage source
+
+rdmd bootDoc\generate.d source
 
 rem goto lol
