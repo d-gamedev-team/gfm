7a685372be9d47fd26e3f5ce5346827f96936220
ponce <aliloko@gmail.com>
2014-12-03 01:21:45 +0100

    Align names on Vertex struct in shaders.

--- examples/simpleshader/simpleshader.d
+++ examples/simpleshader/simpleshader.d
@@ -42,14 +42,14 @@ void main()
         q{#version 330 core
 
         #if VERTEX_SHADER
-        layout(location = 0) in vec4 vertexPos;
-        layout(location = 1) in vec4 vertexUV;
+        layout(location = 0) in vec3 position;
+        layout(location = 1) in vec2 coordinates;
         out vec2 fragmentUV;
         //uniform mat4 mvpMatrix;
         void main()
         {
-            gl_Position = /* mvpMatrix * */ vertexPos;
-            fragmentUV = vertexUV.xy;
+            gl_Position = /* mvpMatrix * */ vec4(position, 1.0);
+            fragmentUV = coordinates;
         }
         #endif
 
