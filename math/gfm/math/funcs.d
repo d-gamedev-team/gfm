e9d09fc3b09a7ab3a39f308231bf904faff5182d
ponce <contact@gamesfrommars.fr>
2015-05-18 14:02:16 +0200

    Deprecate min and max function that already exist in Phobos (std.algorithm). This would create many name conflicts.

--- math/gfm/math/funcs.d
+++ math/gfm/math/funcs.d
@@ -6,20 +6,20 @@
 module gfm.math.funcs;
 
 import std.math,
-	   std.traits,
+       std.traits,
        std.range,
        std.math;
 
 static if( __VERSION__ < 2066 ) private enum nogc = 1;
 
 /// Returns: minimum of a and b.
-@nogc T min(T)(T a, T b) pure nothrow
+deprecated("Use std.algorithm.min instead") @nogc T min(T)(T a, T b) pure nothrow
 {
     return a < b ? a : b;
 }
 
 /// Returns: maximum of a and b.
-@nogc T max(T)(T a, T b) pure nothrow
+deprecated("Use std.algorithm.max instead") @nogc T max(T)(T a, T b) pure nothrow
 {
     return a > b ? a : b;
 }
