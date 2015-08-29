4f39947c5faf12f2b78506f47dfd71656cb9e16c
Guillaume Piolat <guillaume.piolat@gmail.com>
2015-08-29 10:41:34 +0200

    Remove stb_image translation in favor of the excellent imageformats library

--- image/gfm/image/package.d
+++ image/gfm/image/package.d
@@ -1,4 +1,26 @@
 module gfm.image;
 
-public import gfm.image.stb_image,
-              gfm.image.stb_truetype;
+public import gfm.image.stb_truetype;
+
+import imageformats;
+import ae.utils.graphics.image;
+import ae.utils.graphics.color;
+
+/// The one function you probably want to use.
+/// Loads an image from a static array.
+/// Might throw internally.
+/// Throws: $(D STBImageException) on error.
+Image!RGBA loadImage(in void[] imageData)
+{
+    IFImage ifImage = read_image_from_mem(cast(const(ubyte[])) imageData, 4);
+    int width = cast(int)ifImage.w;
+    int height = cast(int)ifImage.h;
+
+    Image!RGBA loaded;
+    loaded.size(width, height);
+    loaded.pixels = cast(RGBA[]) ifImage.pixels; // no pixel copy, GC does the job
+    return loaded;
+}
+
+deprecated("Use loadImage instead") alias stbiLoadImageAE = loadImage;
+
